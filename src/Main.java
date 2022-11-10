de86dbd337c88df106183619c661bbeb01dba8d2
Luke Moon <lukemoon88@yahoo.co.uk>
2022-11-10 10:03:20 +0000

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static int width = 4;
+    static int height = 5;
+
+    static int area = width * height;
+    static double litres = area * 0.25;
+
+    public static void main(String[] args) {
+        System.out.println("This will use " + litres + " litres.");
+    }
+}
