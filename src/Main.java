1c3d1b6f0d586fb715438e6459856ba5c66f20e0
Luke Moon <lukemoon88@yahoo.co.uk>
2022-11-10 11:10:20 +0000

    3rd commit

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
     static double litres = area * 0.25;
 
     public static void main(String[] args) {
-        System.out.println("This will use a lot of...... " + litres + " litres.");
+        System.out.println("This will use a lot of.. " + litres + " litres.");
     }
 }
